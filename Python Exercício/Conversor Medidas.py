0d9feed4918580b86b6bd499f32ff90dbd3ba9b7
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-05-09 13:46:52 -0300

    .
    .

---
+++ Python Exercício/Conversor Medidas.py
@@ -0,0 +1,7 @@
+medida = float(input("Uma distância em metros: "))
+
+centímetros = medida * 100
+
+milimétros = medida * 100
+
+print("A médida de {} metros corresponde a {} centímetros e {} milímetros" .format(medida,centímetros,milimétros))
