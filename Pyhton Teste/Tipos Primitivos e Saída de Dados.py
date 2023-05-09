939749d06648f0435b986fa89634fb2a7790d3b3
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-05-09 11:02:33 -0300

    tipos primitivos
    exemplo de tipos primitivos na linguagem Python

---
+++ Pyhton Teste/Tipos Primitivos e Saída de Dados.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Digite um número:"))
+numero2 = float(input("Digite outro número:"))
+
+resultado = numero1 + numero2 
+print("A soma entre {} e {} vale: {}".format(numero1,numero2,resultado))
+
+
