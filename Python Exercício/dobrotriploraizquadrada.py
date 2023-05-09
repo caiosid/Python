6c5bf3fecffaee19aca0e4da5edcac20e9b536c7
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-05-09 13:07:07 -0300

    .
    .

---
+++ Python Exercício/dobrotriploraizquadrada.py
@@ -0,0 +1,9 @@
+numero = float(input("Digite um número: "))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** 0.5
+
+print("O dobro de {} é: {}".format(numero,dobro))
+print("O tripo de {} é: {}".format(numero,triplo))
+print("A raiz quadrada de, {} é: {}".format(numero,raiz))
