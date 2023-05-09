7d57840c229a092a66d9cacc843c39d2463354f7
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-05-09 12:45:38 -0300

    .
    .

---
+++ Python Exercício/Antecessor e Sucessor.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número: "))
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print("O sucessor de {} é: {}".format(numero,sucessor))
+print("O Antecessor de, {} é, {}".format(numero,antecessor))
