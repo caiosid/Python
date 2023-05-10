de88c0e995504b864274d4d4d425364d9c5cb1cb
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-05-10 14:22:33 -0300

    .
    .

---
+++ Pyhton Teste/Utilizando Módulos.py
@@ -0,0 +1,5 @@
+from math import sqrt
+número = int(input("Digite um número para que possamos calcular a raiz quadrada: "))
+raiz = sqrt(número)
+print("A raiz de {} é igual a {:.2f}".format(número,raiz))
+
