7cfe538807b0b7a81d1c5644a91dbde5fb2eb179
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-05-12 15:56:48 -0300

    Analisador de Texto
    programa que ler o nome do usuário completo e mostra: o nome com todas as letras maiúsculas e minúsculas, quantas letras ao todo (sem considerar espaços) e quantas letras tem o primeiro nome.

--- Exercícios Modúlos Strings/Analisador de Textos.py
+++ Exercícios Modúlos Strings/Analisador de Textos.py
@@ -1,15 +1,11 @@
-#Lê o nome completo da pessoa 
-nomecompleto = str(input("Digite seu nome compelto: "))
-#Mostra o nome com todas as letras maiúsculas e minúsculas
-print(" {} em letras maiúsculas: ".format(nomecompleto.upper()))
-print("{} em letras maiúsculas: ".format(nomecompleto.lower()))
-# Conta o número total de letras (sem considerar espaços)
-númeroletras = str(len(nomecompleto(" "," ")))
-print("{} total de letras (sem espaços): ".format(númeroletras))
-#Conta o número de letras do primeiro nome
-primeironome = nomecompleto.split()[0]
-númeroletras_primeironome = len(primeironome)
-print("{} de letras do primeiro nome: ".format(númeroletras_primeironome))
-
+ 
+nomecompleto = str(input("Digite seu nome compelto: ")).strip()
+print("Analisando seu nome...")
+print(" O seu nome em letras maiúsculas é {}: ".format(nomecompleto.upper()))
+print("O seu nome em letras maiúsculas é {}: ".format(nomecompleto.lower()))
+print("A quantidade de letras do seu nome é {}:".format(len(nomecompleto) - nomecompleto.count(' ')))
+#print("Seu primeiro nome tem {} letras".format(nomecompleto.find(' ')))
+separa = nomecompleto.split()
+print("Seu primeiro nome é {} e ele tem {} letras".format(separa[0], len(separa[0])))
 
 
